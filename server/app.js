cf645a3b96b1d8cd35240a157713fc635cde8fda
planet-akash <akashyadavakash03@gmail.com>
2026-02-03 11:40:31 +0530

    bug fixed

--- server/app.js
+++ server/app.js
@@ -20,17 +20,7 @@ const openai = new OpenAI({
 });
 
 // db
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-// });
-
-// DB test
-const db = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
